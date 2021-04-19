efacaaef7053c34cb92ca49533bffe04403cd461
Nicolás <euclidesnoeuclides@gmail.com>
2021-04-18 21:50:44 -0500

    [fix] operator register serializer and email confirmation

--- apps/users/serializers/operator.py
+++ apps/users/serializers/operator.py
@@ -14,25 +14,13 @@ from .base import UserRegisterSerializer, UserModelSerializer
 class OperatorRegisterSerializer(UserModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-
-    # def update(self, instance, validated_data):
-    #     instance.username = validated_data.get('username')
-    #     instance.email = validated_data.get('email')
-    #     instance.name = validated_data.get('name')
-    #     instance.last_name = validated_data.get('last_name')
-    #     instance.is_enabled = validated_data.get('is_enabled')
-    #     instance.is_admin = validated_data.get('is_admin')
-    #     instance.save()
-    #     return instance
-
-    # def create(self, data):
-    #     data.pop('password_confirmation')
-    #     user = User.objects.create_user(
-    #         **data, is_enabled=False, is_admin=False
-    #     )
-    #     self.send_confirmation_email(user)
-    #     return user
+        fields = [
+            'username',
+            'email',
+            'password',
+            'name',
+            'last_name',
+        ]
 
     def create(self, validated_data: dict) -> User:
         validated_data['is_enabled'] = False
@@ -40,10 +28,10 @@ class OperatorRegisterSerializer(UserModelSerializer):
         user = User(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
-        self.send_confirmation_email(user)
+        self.send_confirmation_email(user, validated_data['password'])
         return user
 
-    def send_confirmation_email(self, user):
+    def send_confirmation_email(self, user, password):
         """
         Send account verification link to new operator.
         """
@@ -52,7 +40,7 @@ class OperatorRegisterSerializer(UserModelSerializer):
         from_email = 'Project Creator <noreply@projectcreator.com>'
         content = render_to_string(
             'users/account_verification.html',
-            {'token': verification_token, 'user': user}
+            {'token': verification_token, 'user': user, 'password': password}
         )
         msg = EmailMultiAlternatives(
             subject, content, from_email, [user.email])
